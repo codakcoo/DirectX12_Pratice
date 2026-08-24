ac2b1c524c74681eb225f8746b06811803adee31
codakcoo <tmdrnjs100@gmail.com>
2026-08-24 23:55:41 +0900

    d3d init - 4.3.2(CreateFence) ~ 4.3.4(CreateCommand)

--- direct3d_init/Init.cpp
+++ direct3d_init/Init.cpp
@@ -1,69 +1,38 @@
-#include <Windows.h>
-#include <d3d12.h>
-#include <dxgi1_4.h>
-#include <wrl.h>
-#include "d3dx12.h"
-#include "d3dUtil.h"
+#include "Init.h"
+#include <assert.h>
 
-HWND gMaindWnd = nullptr;
+Init* Init::mApp = nullptr;						// 정적 멤버는 .cpp에서 정의 필수
+Init* Init::GetApp() { return mApp; }
 
-using Microsoft::WRL::ComPtr;
-
-ComPtr<ID3D12Device> g_device;
-ComPtr<IDXGIFactory4> g_dxgiFactory;
-ComPtr<ID3D12Fence> g_fence;
-ComPtr<ID3D12CommandQueue> g_commandQueue;
-ComPtr<ID3D12CommandAllocator> g_commandAllocator;
-ComPtr<ID3D12GraphicsCommandList> g_commandList;
-ComPtr<IDXGISwapChain> g_swapChain;
-ComPtr<ID3D12DescriptorHeap> g_rtvHeap;
-ComPtr<ID3D12DescriptorHeap> g_dsvHeap;
-ComPtr<ID3D12Resource> g_SwapChainBuffer[2];
-ComPtr<ID3D12Resource> g_depthStencilBuffer;
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
-bool InitWindow(HINSTANCE hInstance);
-bool InitD3D();
-void FlushCommandQueue();
-void Draw();
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+Init::Init(HINSTANCE hInstance) : mhAppInst(hInstance)
 {
-	if (!InitWindow(hInstance)) return 0;
-	if (!InitD3D()) return 0;
-	MSG msg = {};
-	while (msg.message != WM_QUIT)
-	{
-		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-		else
-		{
-			Draw();
-		}
-	}
-	FlushCommandQueue();
-	return static_cast<int>(msg.wParam);
+	assert(mApp == nullptr);	// 유일한 인스턴스만 존재해야 함
+	mApp = this;				// 생성자에서 자신을 등록
+}
+Init::~Init() { mApp = nullptr; }	// 소멸자에서 자신을 해제
+
+// 클래스 밖의 전역 함수 - 이게 Windows에 넘어감
+LRESULT CALLBACK MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	return Init::GetApp()->MsgProc(hwnd, msg, wParam, lParam);
 }
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+LRESULT Init::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg)
 	{
-		case WM_DESTROY:
-			PostQuitMessage(0);
-			return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
 	}
-	return DefWindowProc(hwnd, msg, wParam, lParam);   // 윈도우 기본 처리
+	return DefWindowProc(hwnd, msg, wParam, lParam);
 }
 
-bool InitWindow(HINSTANCE hInstance)
+bool Init::InitWindow(HINSTANCE hInstance)
 {
 	WNDCLASS wc = {};
 	wc.style = CS_HREDRAW | CS_VREDRAW;
-	wc.lpfnWndProc = WndProc;
+	wc.lpfnWndProc = MainWndProc;								// <- 전역 함수이게 정상 등록
 	wc.hInstance = hInstance;
 	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
 	wc.hbrBackground = (HBRUSH)GetStockObject(NULL_BRUSH);
@@ -78,7 +47,7 @@ bool InitWindow(HINSTANCE hInstance)
 		return false;
 	}
 
-	RECT R = { 0, 0, 800, 600 };
+	RECT R = { 0, 0, mClientWidth, mClientHeight };
 	AdjustWindowRect(&R, WS_OVERLAPPEDWINDOW, FALSE);	// 클라이언트 영역 기준 보정
 
 	gMaindWnd = CreateWindow(L"MainWnd", L"Direct3D 12 Init", 
@@ -100,7 +69,16 @@ bool InitWindow(HINSTANCE hInstance)
 	return true;
 }
 
-bool InitD3D()
+bool Init::Initialize()
+{
+	return true;
+}
+int Init::Run()
+{
+	return 1;
+}
+
+bool Init::InitD3D()
 {
 // 디버깅용
 #if defined(DEBUG) || defined(_DEBUG)
@@ -112,9 +90,12 @@ bool InitD3D()
 }
 #endif
 
+	/*
+	* 1. 장치 생성
+	* DXGI 팩토리 생성, 디바이스 생성, 하드웨어 어댑터 생성
+	*/
 	// DXGI 팩토리 생성
 	ThrowIfFailed(CreateDXGIFactory1(IID_PPV_ARGS(&g_dxgiFactory)));
-
 	// 하드웨어 어댑터 자치 생성
 	HRESULT hardwareResult = D3D12CreateDevice(
 		nullptr,								// nullptr = default adapter = 0
@@ -130,18 +111,87 @@ bool InitD3D()
 			pWarpAdapter.Get(),						// WARP adapter
 			D3D_FEATURE_LEVEL_11_0,					// 최소 지원 기능 레벨 dx 11.0
 			IID_PPV_ARGS(&g_device)));				// output device pointer
-
-		return true;
 	}
+
+	/*
+	* 2. Fence 생성, 서술자 크기 가져오기
+	*/
+	// Fence 생성
+	ThrowIfFailed(g_device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&g_fence)));
+	// 서술자 크기 가져오기
+	g_rtvDescriptorSize = g_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	g_dsvDescriptorSize = g_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_DSV);
+	g_cbvSrvUavDescriptorSize = g_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+
+	/*
+	* 3. 4x MSAA 지원 여부 확인
+	*/
+	D3D12_FEATURE_DATA_MULTISAMPLE_QUALITY_LEVELS msQualityLevels;
+	msQualityLevels.Format = mBackBufferFormat;	// 백버퍼 포맷(D3D_DRIVER_TYPE_HARDWARE)
+	msQualityLevels.SampleCount = 4;				// 샘플링 카운트
+	msQualityLevels.Flags = D3D12_MULTISAMPLE_QUALITY_LEVELS_FLAG_NONE; // 멀티샘플링 품질 수준 플래그
+	msQualityLevels.NumQualityLevels = 0;		// 지원되는 멀티샘플링 품질 수준 수
+	ThrowIfFailed(g_device->CheckFeatureSupport(
+		D3D12_FEATURE_MULTISAMPLE_QUALITY_LEVELS,	// 멀티샘플링 품질 수준 확인
+		&msQualityLevels,							// 멀티샘플링 품질 수준 구조체
+		sizeof(msQualityLevels)));					// 구조체 크기
+
+
+	m4xMsaaQuality = msQualityLevels.NumQualityLevels;	// 지원되는 멀티샘플링 품질 수준 수
+	assert(m4xMsaaQuality > 0 && "Unexpected MSAA quality level.");	// 지원되는 멀티샘플링 품질 수준 수가 0이면 오류
+
+
 	return true;
 }
 
-void FlushCommandQueue()
+/*
+* 4. 명령 대기열과 명력 목록 생성
+*/
+void Init::CreateCommandObjects()
+{
+	D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+	queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;		// 명령 대기열 타입
+	queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;		// 명령 대기열 플래그
+	ThrowIfFailed(g_device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&g_commandQueue)));		// 명령 대기열 생성
+
+	ThrowIfFailed(g_device->CreateCommandAllocator(
+		D3D12_COMMAND_LIST_TYPE_DIRECT,				// 명령 목록 타입
+		IID_PPV_ARGS(g_commandAllocator.GetAddressOf())));			// 명령 할당자 생성
+
+	ThrowIfFailed(g_device->CreateCommandList(
+		0,											// 노드 마스크
+		D3D12_COMMAND_LIST_TYPE_DIRECT,				// 명령 목록 타입
+		g_commandAllocator.Get(),					// 명령 할당자
+		nullptr,									// 초기 파이프라인 상태 객체
+		IID_PPV_ARGS(g_commandList.GetAddressOf())));			// 명령 목록 생성
+
+	// 명령 목록은 생성 시점에 열려있으므로, 닫아야 한다.
+	// Reset을 호출하는데, Reset을 호출하려면 명령 목록이 닫혀있어야 한다.
+	g_commandList->Close();
+}
+
+void Init::FlushCommandQueue()
 {
 }
 
-void Draw()
+void Init::Draw()
 {
 }
 
 
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	try
+	{
+		Init theApp(hInstance);
+		if (!theApp.Initialize()) return 0;
+		return theApp.Run();
+	}
+	catch (DxException& e)
+	{
+		MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+		return 0;
+	}
+}
---
+++ direct3d_init/Init.h
@@ -0,0 +1,72 @@
+#pragma once
+#include <Windows.h>
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <wrl.h>
+#include "d3dx12.h"
+#include "d3dUtil.h"
+
+
+using Microsoft::WRL::ComPtr;
+
+
+
+class Init
+{
+public:
+	Init(HINSTANCE hInstance);
+	~Init();
+
+	/*
+	* 원인은 멤버 함수 포인터와 일반 함수 포인터가 다른 타입이기 때문
+	* WndProc을 클래스 멤버로 만들면 컴파일러가 숨겨진 this 매개변수를 하나 더 붙여
+	* Windows는 this가 뭔지 모르니까 이걸 호출할 방법이 없음.
+	* 그래서 WndProc은 반드시 클래스 밖의 일반 함수이거나 static 멤버여야 함
+	*/
+	static Init* GetApp();			// 정적 접근자
+
+	bool Initialize();
+	int Run();
+	// CALLBACK 없음. 그냥 멤버 함수
+	LRESULT MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+	
+
+	bool InitWindow(HINSTANCE hInstance);
+	bool InitD3D();
+	void CreateCommandObjects();
+	void FlushCommandQueue();
+	void Draw();
+
+
+protected:
+
+	static Init* mApp;				// 유일한 인스턴스를 가리킴
+	HINSTANCE mhAppInst = nullptr;
+	HWND mhMainWnd = nullptr;
+
+	HWND gMaindWnd = nullptr;
+
+	ComPtr<ID3D12Device> g_device;
+	ComPtr<IDXGIFactory4> g_dxgiFactory;
+	ComPtr<ID3D12Fence> g_fence;
+	ComPtr<ID3D12CommandQueue> g_commandQueue;
+	ComPtr<ID3D12CommandAllocator> g_commandAllocator;
+	ComPtr<ID3D12GraphicsCommandList> g_commandList;
+	ComPtr<IDXGISwapChain> g_swapChain;
+	ComPtr<ID3D12DescriptorHeap> g_rtvHeap;
+	ComPtr<ID3D12DescriptorHeap> g_dsvHeap;
+	ComPtr<ID3D12Resource> g_SwapChainBuffer[2];
+	ComPtr<ID3D12Resource> g_depthStencilBuffer;
+
+	UINT g_rtvDescriptorSize = 0;
+	UINT g_dsvDescriptorSize = 0;
+	UINT g_cbvSrvUavDescriptorSize = 0;
+
+	DXGI_FORMAT mBackBufferFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+	DXGI_FORMAT mDepthStencilFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	int mClientWidth = 800;
+	int mClientHeight = 600;
+
+	bool m4xMsaaState = false; // 4X MSAA enabled
+	UINT m4xMsaaQuality = 0; // quality level of 4X MSAA
+};
